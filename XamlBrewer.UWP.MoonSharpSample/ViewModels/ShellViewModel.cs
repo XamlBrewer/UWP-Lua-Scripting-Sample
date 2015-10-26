b8cb7a3505bd8ba7dc460e405d235c8b58539eab
Diederik Krols <diederik_krols@hotmail.com>
2015-10-26 22:18:19 +0100

    Expression Evaluation.

---
+++ XamlBrewer.UWP.MoonSharpSample/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using XamlBrewer.UWP.MoonSharpSample.Views;
+
+namespace Mvvm
+{
+    class ShellViewModel : ViewModelBase
+    {
+        public ShellViewModel()
+        {
+            // Build the menu
+            Menu.Add(new MenuItem() { Glyph = "\uE1D0", Text = "Expressions", NavigationDestination = typeof(ExpressionPage) });
+            //Menu.Add(new MenuItem() { Glyph = "\uE2B1", Text = "Templated", NavigationDestination = typeof(TemplatePage) });
+            //Menu.Add(new MenuItem() { Glyph = "\uE114", Text = "Camera", NavigationDestination = typeof(CameraPage) });
+        }
+    }
+}
---
+++ XamlBrewer.UWP.MoonSharpSample/Views/ExpressionPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using MoonSharp.Interpreter;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace XamlBrewer.UWP.MoonSharpSample.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ExpressionPage : Page
+    {
+        public ExpressionPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var script = "return " + Expression.Text;
+
+            try
+            {
+                var result = Script.RunString(script);
+                Result.Foreground = new SolidColorBrush(Colors.Black);
+                Result.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                Result.Foreground = new SolidColorBrush(Colors.Red);
+                Result.Text = ex.Message;
+            }
+        }
+    }
+}
