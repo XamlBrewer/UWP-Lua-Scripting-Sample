046446e85e31bf46ff1859dbb59b15068054fcbc
Diederik Krols <diederik_krols@hotmail.com>
2015-10-27 22:46:58 +0100

    Cosmetics.

--- XamlBrewer.UWP.MoonSharpSample/ViewModels/ShellViewModel.cs
+++ XamlBrewer.UWP.MoonSharpSample/ViewModels/ShellViewModel.cs
@@ -8,10 +8,10 @@ namespace Mvvm
         {
             // Build the menu
             Menu.Add(new MenuItem() { Glyph = "\uE1D0", Text = "Expressions", NavigationDestination = typeof(ExpressionPage) });
-            Menu.Add(new MenuItem() { Glyph = "\uE2B1", Text = "Lua Functions", NavigationDestination = typeof(LuaFunctionPage) });
-            Menu.Add(new MenuItem() { Glyph = "\uE114", Text = "C# Functions", NavigationDestination = typeof(CSharpFunctionPage) });
-            Menu.Add(new MenuItem() { Glyph = "\uE114", Text = "Object Sharing", NavigationDestination = typeof(ObjectSharingPage) });
-            Menu.Add(new MenuItem() { Glyph = "\uE114", Text = "Event Handling", NavigationDestination = typeof(EventHandlingPage) });
+            Menu.Add(new MenuItem() { Glyph = "\uE2B3", Text = "Lua Functions", NavigationDestination = typeof(LuaFunctionPage) });
+            Menu.Add(new MenuItem() { Glyph = "\uE2B4", Text = "C# Functions", NavigationDestination = typeof(CSharpFunctionPage) });
+            Menu.Add(new MenuItem() { Glyph = "\uE1CA", Text = "Object Sharing", NavigationDestination = typeof(ObjectSharingPage) });
+            Menu.Add(new MenuItem() { Glyph = "\uE25C", Text = "Events", NavigationDestination = typeof(EventHandlingPage) });
         }
     }
 }
